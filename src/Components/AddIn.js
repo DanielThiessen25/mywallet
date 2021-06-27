c9f55f7aec20ffca8d6d406f3b3c0ab0da03d276
Daniel Luiz <danielluizthiessen@gmail.com>
2021-06-27 14:57:28 -0300

    Implementei layout de adicionar entradas e saidas

---
+++ src/Components/AddIn.js
@@ -0,0 +1,86 @@
+import Screen from "./Screen";
+import { useState } from 'react';
+import styled from 'styled-components';
+
+export default function AddIn() {
+    const [amount, setAmount] = useState('');
+    const [description, setDescription] = useState('');
+
+    return (
+
+        <Screen>
+            <Heading>
+                Nova entrada
+            </Heading>
+            <InputArea>
+                <input placeholder="Valor" required type="text" value={amount} onChange={e => setAmount(e.target.value)} />
+                <input placeholder="Descrição" required type="text" value={description} onChange={e => setDescription(e.target.value)} />
+                <button>Salvar entrada</button>
+            </InputArea>
+            <Fill></Fill>
+        </Screen>
+    );
+}
+
+const Heading = styled.div`
+    width: 100%;
+    height: 32px;
+    display: flex;
+    flex-direction: row;
+    align-items: center;
+    justify-content: space-between;
+    font-family: 'Raleway', sans-serif;
+    font-style: normal;
+    font-weight: bold;
+    font-size: 26px;
+    line-height: 31px;
+    color: #FFFFFF;
+    margin-bottom: 23px;
+`;
+
+const InputArea = styled.div`
+    width: 100%;
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    font-family: 'Raleway', sans-serif;
+    font-style: normal;
+    font-weight: bold;
+    font-size: 15px;
+    line-height: 18px;
+    color: #FFFFFF;
+
+    input{
+        width: 100%;
+        height: 60px;
+        border: none;
+        padding: 18px 0 18px 15px;
+        font-family: 'Raleway', sans-serif;
+        font-style: normal;
+        font-weight: normal;
+        font-size: 20px;
+        line-height: 23px;
+        color: #000000;
+        margin-bottom: 15px;
+        border-radius: 5px;
+    }
+    button{
+        width: 100%;
+        height: 46px;
+        background: #A328D6;
+        border-radius: 5px;
+        border: none;
+        font-family: 'Raleway', sans-serif;
+        font-style: normal;
+        font-weight: bold;
+        font-size: 20px;
+        line-height: 23px;
+        color: #FFFFFF;
+        margin-bottom: 36px;
+    }
+`;
+
+const Fill = styled.div`
+    width: 100%;
+    height: 100%;
+`;
--- src/Components/App.js
+++ src/Components/App.js
@@ -3,6 +3,8 @@ import { BrowserRouter, Switch, Route } from "react-router-dom";
 import Login from './Login';
 import SignUp from './SignUp';
 import Welcome from './Welcome';
+import AddIn from "./AddIn";
+import AddOut from "./AddOut";
 
 export default function App() {
     return (
@@ -11,6 +13,8 @@ export default function App() {
                     <Route path="/" exact={true} component={Login} />
                     <Route path="/sign-up" exact={true} component={SignUp} />
                     <Route path="/welcome" exact={true} component={Welcome} />
+                    <Route path="/nova-entrada" exact={true} component={AddIn} />
+                    <Route path="/nova-saida" exact={true} component={AddOut} />
             </Switch>
         </BrowserRouter>
 
