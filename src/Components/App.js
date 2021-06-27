b28e3f73a8bb6d9714f4d9656493872c03ddc0b9
Daniel Luiz <danielluizthiessen@gmail.com>
2021-06-27 01:57:40 -0300

    Implementei o layout da pagina inicial de registros

--- src/Components/App.js
+++ src/Components/App.js
@@ -2,7 +2,7 @@ import React from 'react';
 import { BrowserRouter, Switch, Route } from "react-router-dom";
 import Login from './Login';
 import SignUp from './SignUp';
-
+import Welcome from './Welcome';
 
 export default function App() {
     return (
@@ -10,6 +10,7 @@ export default function App() {
             <Switch>
                     <Route path="/" exact={true} component={Login} />
                     <Route path="/sign-up" exact={true} component={SignUp} />
+                    <Route path="/welcome" exact={true} component={Welcome} />
             </Switch>
         </BrowserRouter>
 
